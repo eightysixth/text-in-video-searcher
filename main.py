4ca9668fe2801b08d9bed6db5a625bead7553444
Vivek Acharya <rec@vach.ca>
2020-01-18 17:19:00 -0700

    sequence prediction

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ import re;
 
 word_list = []
 
+def writeToJsonFile(fileName, data):
+    with open(fileName, 'w', encoding='utf8') as f:
+        json.dump(data, f, indent=4)
+
+
+
 
 def runOCR(video_file_name):
     cap = cv2.VideoCapture(video_file_name);
@@ -28,9 +34,10 @@ def runOCR(video_file_name):
             medianBlur = cv2.medianBlur(gray, 3)
             data = pytesseract.image_to_string(gray)
 
-            for word in data.split(' '):
+            for word in data.split("/w+"):
                 if not word in text_output:
                     text_output[word] = []
+                print(word)
                 text_output[word].append(frame_id)
             # cv2.imshow('frame',frame)
             print("frame #", frame_id, data)
@@ -38,9 +45,33 @@ def runOCR(video_file_name):
             # out.write(gray)
         else:
             break;
+
+    writeToJsonFile(video_file_name + '.original.json', text_output)
     
-    with open(video_file_name + '.json', 'w', encoding='utf8') as f:
-        json.dump(text_output, f, indent=4)
+    for word in text_output:
+        # Iterate through the frames and determine if they order in sequence, if they do, remove sequence
+        good_frames = []
+        cur_frame = text_output[word][0]; # First element, eg. frame 104
+        next_predicted = cur_frame + 1; # Next prediction is 104 + 1 = 105
+        start_index = 1
+        try:
+            word.index(start_index);
+            for frame_no in text_output[word]:
+            if frame_no == next_predicted:
+                next_predicted = next_predicted + 1;
+            else:
+                good_frames.append(cur_frame)
+                good_frames.append(frame_no)
+                
+                cur_frame = frame_no
+                next_predicted = cur_frame + 1;
+        except ValueError as e:
+            continue;
+        
+        text_output[word] = good_frames
+
+    writeToJsonFile(video_file_name + '.trimmed.json', text_output)
+
     cv2.destroyAllWindows();
     cap.release();
     # out.release();
@@ -88,6 +119,8 @@ def main():
 
 # main();
 # ffmpeg -i <input> -filter:v fps=fps=30 <output>
+
+# Test video: https://www.youtube.com/watch?v=5paISomtdn4
 
 def test():
     runOCR("out.K1Jkrngi0R0.mp4")
